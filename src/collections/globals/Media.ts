d426f610e65b9c188b0fa31612d26c063f2647f2
oliviervd <oliviervandhuynslager@gmail.com>
2024-03-13 07:51:28 +0100

    add init global collection: Media

---
+++ src/collections/globals/Media.ts
@@ -0,0 +1,34 @@
+import {CollectionConfig} from "payload/types";
+
+const Media:CollectionConfig = {
+    slug: 'media',
+    admin: {
+        useAsTitle: 'title',
+        group: "globals"
+    },
+    upload: {
+        staticURL: "/media",
+        staticDir: "/media",
+        disableLocalStorage: true,
+        // todo: add admin thumbnail
+        mimeTypes: ["image/*"]
+    },
+    access: {
+        read: () => true,
+        create: () => true,
+        delete: () => true,
+        update:() => true,
+    },
+    fields: [
+        {
+            name: 'title',
+            type: "text"
+        },
+        {
+            name: 'alt',
+            type: "text"
+        }
+    ]
+}
+
+export default Media
