faf9a18793e6a8a4b056e74673915fd40caf42c7
oliviervd <oliviervandhuynslager@gmail.com>
2024-03-18 17:46:15 +0100

    add init collection Text

--- src/collections/blocks/Text.ts
+++ src/collections/blocks/Text.ts
@@ -4,16 +4,6 @@ const RichText:Block = {
     slug: "richRext",
     fields: [
         {
-            // type of text
-            label: "type",
-            name: "type",
-            type: "select",
-            options: [
-                "about",
-                "article"
-            ]
-        },
-        {
             // actual text
             label:"text",
             name: "text",
---
+++ src/collections/copy/article.ts
@@ -0,0 +1,28 @@
+import {CollectionConfig} from "payload/types";
+import RichText from "../blocks/Text";
+
+const Text:CollectionConfig = {
+    slug: "text",
+    fields: [
+        {
+            // type of text
+            label: "type",
+            name: "type",
+            type: "select",
+            options: [
+                "about",
+                "article"
+            ]
+        },
+        {
+            name: 'layout', // required
+            type: 'blocks', // required
+            blocks: [
+                // add required block here
+                RichText,
+            ]
+        }
+    ]
+
+}
+export default Text
