1a3fcc818c14e815990d53cf13c6955d72f4bba7
oliviervd <oliviervandhuynslager@gmail.com>
2024-03-24 09:02:22 +0100

    debug S3 Adapter

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -12,37 +12,36 @@ import Media from './collections/globals/Media'
 import Globals from "./collections/globals/Globals";
 import Text from "./collections/copy/Text";
 
-// config adapter to connect with S3 storage
-const adapter = s3Adapter({
-  config: {
-    endpoint: process.env.S3_ENDPOINT,
-    region: process.env.S3_REGION,
-    credentials: {
-      accessKeyId: process.env.S3_ACCESS_KEY_ID,
-      secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
-    }
-  },
-  bucket: process.env.S3_BUCKET
-})
 
 
 export default buildConfig({
   //adapter for cloud storage
   plugins: [
-      cloudStorage({
-        collections: {
-          // collections to attach the adapter to
-          'media': {
-            adapter,
-            generateFileURL: ({ filename, prefix }) => {
-              return ["https://d3b71b8mgnztvw.cloudfront.net", prefix, filename]
-                  .filter(Boolean)
-                  .join("/")
-            }
-          }
+    cloudStorage({
+      collections: {
+        media: {
+          adapter: s3Adapter({
+            config: {
+              endpoint: process.env.S3_ENDPOINT,
+              region: process.env.S3_REGION,
+              credentials: {
+                accessKeyId: process.env.S3_ACCESS_KEY_ID,
+                secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+              },
+            },
+            bucket: process.env.S3_BUCKET,
+          }),
+          // https://d3b71b8mgnztvw.cloudfront.net - cloudfront
+          generateFileURL: ({ filename, prefix }) => {
+            return ["https://d3b71b8mgnztvw.cloudfront.net", prefix, filename]
+                .filter(Boolean)
+                .join("/");
+          },
         },
-      })
+      },
+    }),
   ],
+
   // adapter for mongoDB
   db: mongooseAdapter({
     url: process.env.DATABASE_URI,
